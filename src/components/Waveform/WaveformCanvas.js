f36ccb888b935d5a3d4e2e60773c1619e2cc8348
mx <uhlich@ualberta.ca>
2019-02-26 13:16:28 -0700

    beeps synthesized by tone.js in a webview underneath the canvas. works pretty well right now. will most likely want to combine the canvas and the webview, since i think canvas is just using a webview under the hood anyway. this should improve performance further, if its possible to do so. that will probably be the next task.

--- src/components/Waveform/WaveformCanvas.js
+++ src/components/Waveform/WaveformCanvas.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, View} from "react-native";
+import {WebView, StyleSheet, View} from "react-native";
 import Canvas from 'react-native-canvas';
 
 import { waveformData } from './WaveformData.js';
@@ -13,26 +13,27 @@ class WaveformCanvas extends React.Component {
     console.log('constructor')
     super();
     this.state = {
-      x:0,
-      y:50,
+      //x:0,
+      //y:50,
       //canvas:null,
-      path:"",
+      //path:"",
       //front:"-1,50 -1,50",
       //back:"198,0 200,0",
       dimensions:undefined
     };
-    this.path = ""
+    //this.path = ""
     //this.path = "0,152 1,152 2,153 3,155 4,153 5,155 6,152 7,153 8,155 9,157 10,157 11,159 12,162 13,165 14,165 15,168 16,174 17,178 18,178 19,170 20,158 21,145 22,126 23,102 24,70 25,40 26,20 27,12 28,22 29,52 30,96 31,138 32,163 33,172 34,170 35,163 36,158 37,158 38,159 39,160 40,158"
     //this.path = "5,158 10,155 15,155 20,152 25,154 30,152 35,145 40,144 45,145 50,145 55,153 60,158 65,160 70,160 75,157 80,152 85,155 90,157 95,168 100,158 105,40 110,96 115,163 120,158 125,159 130,158 135,158 140,155 145,159 150,160 155,161 160,159 165,161 170,157 175,159 180,160 185,161 190,161 195,159 200,163 205,162 210,163 215,163 220,159 225,153 230,149 235,148 240,149 245,149 250,150 255,152 260,150 265,152 270,152 275,155 280,156 285,153 290,155 295,155 300,157 305,158 310,155 315,155 320,152 325,154 330,152 335,145 340,144 345,145 350,145 355,153 360,158 365,160 370,160 375,157 380,152 385,155 390,157 395,168 400,158 405,40 410,96 415,163 420,158 425,159 430,158 435,158 440,155 445,159 450,160 455,161 460,159 465,161 470,157 475,159 480,160 485,161 490,161 495,159 500,163 505,162 510,163 515,163 520,159 525,153 530,149 535,148 540,149 545,149 550,150 555,152"
     //this.path2 = "5,158 10,155 15,155 20,152 25,154 30,152 35,145 40,144 45,145 50,145 55,153 60,158 65,50 70,50 75,50 80,152 85,155 90,157 95,168 100,158 105,40 110,96 115,163 120,158 125,159 130,158 135,158 140,155 145,159 150,160 155,161 160,159 165,161 170,157 175,159 180,160 185,161 190,161 195,159 200,163 205,162 210,163 215,163 220,159 225,153 230,149 235,148 240,149 245,149 250,150 255,152 260,150 265,152 270,152 275,155 280,156 285,153 290,155 295,155 300,157 305,158 310,155 315,155 320,152 325,154 330,152 335,145 340,144 345,145 350,145 355,153 360,158 365,160 370,160 375,157 380,152 385,155 390,157 395,168 400,158 405,40 410,96 415,163 420,158 425,159 430,158 435,158 440,155 445,159 450,160 455,161 460,159 465,161 470,157 475,159 480,160 485,161 490,161 495,159 500,163 505,162 510,163 515,163 520,159 525,153 530,149 535,148 540,149 545,149 550,150 555,152"
     //this.front = [];
     //this.back = [[201,0], [202, 0]];
     this.x = 0;
+    this.y = 0;
     this.arrIndex = 0;
-    this.y = 50;
-    this.begin = Date.now();
     this.renderFrame = this.renderFrame.bind(this);
     this.traceWidth = 10;
+    this.beeptime = 0;
+    this.pitches = ['D2', 'D#2', 'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2', 'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3', 'A#3', 'B3', 'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4', 'C5', 'C#5', 'D5', 'D#5', 'E5', 'F5', 'F#5']
     //this.stepsize = this.props.stepsize; //stepsize must be smaller than traceWidth otherwise we won't clear everything.
     //this.handleCanvas = this.handleCanvas.bind(this);
     //this.userTest = React.createRef();
@@ -78,13 +79,13 @@ class WaveformCanvas extends React.Component {
       this.ctx.strokeStyle = this.props.colour;
       this.ctx.fillStyle = this.props.colour;
     }
+  }
 
-    //Not called yet
-    //console.log('handleCanvas')
-    //console.log("render " + this.frameId)
-    //this._frameId = requestAnimationFrame(() => {
-    //  this.renderFrame();
-    //});
+  handleWebView = (webview) => {
+    console.log('handleWebView')
+    if(webview){
+      this.webviewRef = webview;
+    }
   }
 
   ecgGenerator(x) {
@@ -105,18 +106,18 @@ class WaveformCanvas extends React.Component {
     //var scaled =  (w.range.max -  p) / (w.range.max - w.range.min) * this.state.dimensions.height
     //return scaled.toFixed();
 
-    var s = store.getState();
+    //var s = store.getState();
     switch (this.props.wavetype) {
       case "HR":
-        switch (s.Waveform) {
+        switch (this.waveform) {
           case "Normal Sinus Rhythm":
             var w = waveformData.HR.NSR;
             //c = Math.floor(this.state.dimensions.width / (5000 * (store.getState().HeartRate / 60000)));
             
-            var bps = store.getState().HeartRate/60                     //beats per second
+            var bps = this.heartrate/60                     //beats per second
             var bpw = this.spw*bps                                      //beats per window
             var ppb = this.state.dimensions.width/bpw                   //pixels per beat
-            var fpb = (this.state.dimensions.width/this.stepsize)/bpw   //frames per beat
+            //var fpb = (this.state.dimensions.width/this.stepsize)/bpw   //frames per beat
             //console.log("bps " + bps)
             //console.log("bpw " + bpw)
             //console.log("ppb " + ppb)
@@ -146,33 +147,28 @@ class WaveformCanvas extends React.Component {
         cadence = Math.max(cadence,28)
         //console.log("cadence: " + cadence)
 
-        /*if (cadence>w.nPoints){
-          var xPos = x % cadence
-          if (xPos>w.nPoints)
-            var p = waveformData.NOISE.NORMAL.dataPoints[xPos % waveformData.NOISE.NORMAL.nPoints]
-          else
-            var p = w.dataPoints[xPos];
-
-          var difference = cadence-w.nPoints;
-          var noise = waveformData.NOISE.NORMAL.dataPoints;
-          var extended = 
-          var p = extended[x % cadence];
-        } else
-          var p = w.dataPoints[x % cadence];*/
-
         // If cadence is really high, we will add noise to the end
-        var xPos = x % cadence
+        var xPos = (x % cadence) + Math.floor(Math.random()*2)
         if (xPos>w.nPoints)
           var p = waveformData.NOISE.NORMAL.dataPoints[xPos % waveformData.NOISE.NORMAL.nPoints]
         else
           var p = w.dataPoints[xPos];
 
-        var scaled =  (w.range.max -  p) / (w.range.max - w.range.min) * this.state.dimensions.height
-        return scaled.toFixed();
+        var scaled =  ((w.range.max -  p) / (w.range.max - w.range.min) * this.state.dimensions.height).toFixed();
+
+        // Beep if necessary
+        if ((scaled < this.threshold) && ((x - this.beeptime)>(3*this.stepsize))){
+          this.beeptime = x;
+          //this.webviewRef.injectJavaScript('beep(\'D5\')');
+          this.webviewRef.injectJavaScript('beep(\'' + this.pitch + '\')');
+        }
+        //console.log(scaled)
+        //(s.O2Sat-96)
+        return scaled;
         break;
 
       case "BP":
-        if (s.Waveform == "Compressions In-Progress") {
+        if (this.waveform == "Compressions In-Progress") {
           return this.state.dimensions.height /2;
         } else {
           return x % 200;
@@ -209,20 +205,17 @@ class WaveformCanvas extends React.Component {
       if (this.state.dimensions){
 
         this.y = this._waveform(this.arrIndex);
-        var newPoint = this.x + "," + this.y + " "
-        this.path += newPoint
+        //var newPoint = this.x + "," + this.y + " "
+        //this.path += newPoint
 
         //console.log("newPoint: " + newPoint)
 
-        //this.ctx.clearRect(this.x, 0, this.traceWidth, this.state.dimensions.height);
-
         // Draw a particular line segment of our curve.
         if(this.x===0){
-          this.wintimer = new Date()
+          //this.wintimer = new Date()
           this.ctx.moveTo(this.x, this.y);
           this.ctx.fillRect(this.x, this.y, 1, 1);
         } else {
-          //this.ctx.fillRect(this.x, this.y, 1, 1);
           this.ctx.lineTo(this.x, this.y);
           this.ctx.stroke();
         }
@@ -233,18 +226,6 @@ class WaveformCanvas extends React.Component {
         //console.log("overage: " + overage)
         if (overage > 0)
           this.ctx.clearRect(0, 0, overage, this.state.dimensions.height);
-
-        /*this.ctx.beginPath();
-        this.ctx.moveTo(0, 150);
-        this.ctx.lineTo(29, 150);
-        this.ctx.stroke();
-
-        this.ctx.beginPath();
-        this.ctx.moveTo(1, 100);
-        this.ctx.lineTo(30, 100);
-        this.ctx.stroke();
-        
-        this.ctx.fillRect(this.x, 0, 1, 200);*/
         
         this.x = (this.x % (this.state.dimensions.width+1)) + this.stepsize
 
@@ -252,11 +233,11 @@ class WaveformCanvas extends React.Component {
           //console.log("path: " + this.path)
           //this.ctx.clearRect(0, 0, this.state.dimensions.width, this.state.dimensions.height);
           this.ctx.beginPath();
-          this.path = ""
+          //this.path = ""
           this.x = 0
-          this.wintimer2 = new Date()
-          this.spw = (this.spw + ((this.wintimer2.getTime() - this.wintimer.getTime())/1000))/2
-          console.log("this.spw: " + this.spw)
+          //this.wintimer2 = new Date()
+          //this.spw = (this.spw + ((this.wintimer2.getTime() - this.wintimer.getTime())/1000))/2
+          //console.log("this.spw: " + this.spw)
         }
 
         if(this.x <= this.state.dimensions.width){
@@ -265,122 +246,6 @@ class WaveformCanvas extends React.Component {
       }
     }
 
-
-
-
-
-
-
-
-
-
-
-    //console.log("renderFrame " + this._frameId)
-    /*now = Date.now();
-    delta = now - this.then;
-
-    if (delta > this.interval) {
-    //if (true) {
-      //console.log(">>>renderFrame interval")
-      this.then = now - (delta % this.interval);
-
-      const stepsize = 1;
-
-      //if (this.state.dimensions){
-      //if (true) {
-
-        
-        this.y = this._waveform(this.arrIndex);
-        
-        if (this.x < (this.state.dimensions.width+9)) {
-
-          //this.x = this.x;
-          //this.y = y;
-
-          var newPoint = this.x + "," + this.y + " "
-          //console.log("newPoint: " + newPoint)
-
-          this.path += newPoint
-
-          //if (this._frameId % 2 === 0)
-            console.log("x: " + this.x)
-            this.setState({path:this.path});
-          //else
-          //  this.setState({path:this.path2});  
-
-
-          //console.log("path: " + this.path)
-
-
-          this.x = this.x + stepsize;
-
-          this.arrIndex += stepsize;
-
-        } else {
-          //var temp = this.path
-          //this.path = this.path2
-          //this.path2 = temp
-
-          console.log("path: " + this.path);
-          this.x = 0;
-          this.y = 50;
-          //this.y = this.state.dimensions.height /2;
-          //this.back = this.front;
-          //this.front = [[-1,this.y], [-1,this.y]];
-          this.path = ""
-
-          console.log("x: " + this.x)
-          this.setState({path:this.path});
-
-          this.x = this.x + stepsize;
-
-          this.arrIndex += stepsize;
-        }*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*x = this.x + stepsize;
-
-        this.arrIndex += stepsize;
-        y = this._waveform(this.arrIndex);
-
-        if (x < this.state.dimensions.width) {
-          this.x = x;
-          this.y = y;
-          this.front.push([x, y]);
-          if (this.back.length > 2) {
-            this.back.shift()
-          }
-          if (this.front.length >= 2 && this.back.length >= 2) {
-            this.setState({front:this.front.join(' '), back:this.back.join(' ')});
-          }
-        } else {
-          this.x = 0;
-          this.y = this.state.dimensions.height /2;
-          this.back = this.front;
-          this.front = [[-1,this.y], [-1,this.y]];
-        }*/
-
-
-
-
-
-
-      //}
-    //}
     this._frameId = requestAnimationFrame(this.renderFrame);
   }
 
@@ -391,12 +256,11 @@ class WaveformCanvas extends React.Component {
       if (width == this.state.dimensions.width && height == this.state.dimensions.height) return;
     }
     this.setState({ dimensions: { width:width, height:height } })
-    //this.front = [];
-    //this.back = [[width+1, height/2], [width+1, height/2]];
 
     console.log("width: " + width)
     console.log("height: " + height)
-    
+    this.threshold = Math.round(0.48913*height)
+    console.log("threshold: " + this.threshold)
     
     /*this.fps = 30;
     this.then = Date.now();
@@ -411,18 +275,32 @@ class WaveformCanvas extends React.Component {
     //seconds per window
     this.spw = (width/this.stepsize)/this.fps;
     console.log("seconds per window: " + this.spw)
-    var bps = store.getState().HeartRate/60                     //beats per second
+    var bps = this.heartrate/60                     //beats per second
     var bpw = this.spw*bps                                      //beats per window
     console.log("bpw: " + bpw + " " + Math.round(bpw))
   }
 
 //ref={(c) => {this.canvasRef = c; this.canvasRef.height = this.state.dimensions.height;}} />
+//<Canvas ref={this.handleCanvas}/> 
   render() {
     console.log('render ')
+    var s = store.getState();
+    this.waveform = s.Waveform;
+    console.log("this.waveform: " + this.waveform)
+    this.heartrate = s.HeartRate;
+    console.log("this.heartrate: " + this.heartrate)
+    this.pitch = this.pitches[s.O2Sat % 60]
     if (this.state.dimensions) {
       //console.log("this.state.dimensions: " + this.state.dimensions.height + " " + this.state.dimensions.width)
       return (
         <View style={{ flex: 1, alignSelf: 'stretch' }} onLayout={this.onLayout} > 
+          <WebView
+            ref={this.handleWebView}
+            source={require('./tone_synth.html')}
+            style={{flex: 1, alignSelf: 'stretch' }}
+            javaScriptEnabled={true}
+            domStorageEnabled={true}
+          />
           <Canvas ref={this.handleCanvas}/> 
         </View>
       );
