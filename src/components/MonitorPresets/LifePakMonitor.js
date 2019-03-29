b9be3a86245fe3786c9865defb5f0d5598df3a66
mx <uhlich@ualberta.ca>
2019-03-29 14:12:50 -0600

    fixed lifepakmonitor. now it shows the curves

--- src/components/MonitorPresets/LifePakMonitor.js
+++ src/components/MonitorPresets/LifePakMonitor.js
@@ -10,58 +10,53 @@ export default class LikePakMonitor extends React.PureComponent {
     render() {
         return (
         <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={textStyle.hrText}>HR</Text>
-                <View style={{marginLeft: '42.5%'}}>
+            <View style={styles.dataSection}>
+                <View style={styles.heartRateSection}>
+                    <View style={styles.heartRateData}>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', backgroundColor: 'grey'}}>
+                            <Text style={textStyle.hrText}>HR</Text>
+                            <Text style={[textStyle.o2SatText, {marginRight: '2%'}]}></Text>
+                        </View>
+                        <View style={{flex: 4, flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <Icon
+                                type="FontAwesome"
+                                name="bell"
+                                style={{color: '#80ff80', fontSize: moderateScale(30), marginTop: '2%'}} />
+                            <Text style={[textStyle.vitalValueText, testStyle.vitalText,{color: '#80ff80'}]}>
+                                {this.props.heartRate}
+                            </Text>
+                        </View>
+                    </View>
                 </View>
+
+                <View style={styles.o2SatSection}>
+                    <View style={styles.o2SatData}>
+                        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', backgroundColor: 'grey'}}>
+                            <Text style={textStyle.o2SatText}>SpO2</Text>
+                            <Text style={[textStyle.o2SatText, {marginRight: '2%'}]}>%</Text>
+                        </View>
+                        <View style={{flex: 4, flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <Icon
+                                type="FontAwesome"
+                                name="bell"
+                                style={{color: '#15f4ee', fontSize: moderateScale(30), marginTop: '2%'}} />
+                            <Text style={[textStyle.vitalValueText, testStyle.vitalText, {color: '#15f4ee'}]}>
+                                {this.props.O2Sat}
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+
+                <View style={styles.co2Section}>
+                </View>
+
             </View>
 
-            <View style={styles.heartRateSection}>
-                <View style={styles.heartRateData}>
-                    <Icon
-                        type="FontAwesome"
-                        name="bell"
-                        style={{color: '#80ff80', fontSize: moderateScale(30), marginTop: '2%'}} />
-                    <Text style={[textStyle.vitalValueText, testStyle.vitalText,{color: '#80ff80'}]}>
-                        {this.props.heartRate}
-                    </Text>
-                </View>
-
+            <View style={styles.waveformSection}>
                 <View style={styles.heartRateWave}>
-                    {/*<WaveformCanvas wavetype="HR" colour="green" fps={30} stepsize={5} f={this.props.heartRate}/>*/}
+                        {<WaveformCanvas wavetype="HR" colour="green" fps={30} stepsize={4} f={this.props.heartRate}/>}
                 </View>
-            </View>
-
-            <View style={styles.o2SatSection}>
-                <View style={styles.o2SatData}>
-                    <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', backgroundColor: 'grey'}}>
-                        <Text style={textStyle.o2SatText}>SpO2</Text>
-                        <Text style={[textStyle.o2SatText, {marginRight: '2%'}]}>%</Text>
-                    </View>
-                    <View style={{flex: 4, flexDirection: 'row', justifyContent: 'space-between'}}>
-                        <Icon
-                            type="FontAwesome"
-                            name="bell"
-                            style={{color: '#15f4ee', fontSize: moderateScale(30), marginTop: '2%'}} />
-                        <Text style={[textStyle.vitalValueText, testStyle.vitalText, {color: '#15f4ee'}]}>
-                        {this.props.O2Sat}
-                        </Text>
-                    </View>
-                </View>
-
-                <View style={styles.o2SatWave}>      
-                    {/*<WaveformCanvas wavetype="O2Sat" colour="#15f4ee" fps={30} stepsize={2} f={this.props.heartRate}/>*/}
-                </View>
-            </View>
-
-            <View style={styles.co2Section}>
-                <View style={styles.co2Data}>
-
-                </View>
-
-                <View style={styles.co2Wave}>
-                {/* TODO: Place co2 renderer here. */}
-                </View>
+                <View style={{flex: 1}}></View>
             </View>
         </View>
         );
@@ -81,7 +76,7 @@ const textStyle = StyleSheet.create({
         fontSize: moderateScale(25),
         fontWeight: 'bold',
         color: '#80ff80',
-        marginLeft: '0.5%'
+        marginLeft: '2.5%'
     },
     o2SatText: {
         fontSize: moderateScale(25),
@@ -97,36 +92,45 @@ const textStyle = StyleSheet.create({
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        flexDirection: 'column',
-        backgroundColor: 'black'    
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
     header: {
-        flex: 1,
         flexDirection: 'row',
         justifyContent: 'flex-start',
         backgroundColor: 'grey'
     },
+    waveformSection: {
+        flex: 3.7,
+        flexDirection: 'column',
+        backgroundColor: 'black'
+    },
+    dataSection: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: 'black'
+    },
     heartRateSection: {
-        flex: 4,
-        flexDirection: 'row',
+        flex: 1,
+        flexDirection: 'column',
     },
     o2SatSection: {
-        flex: 5,
-        flexDirection: 'row'
+        flex: 1,
+        flexDirection: 'column'
     },
     co2Section: {
-        flex: 4,
-        flexDirection: 'row'
+        flex: 1,
+        flexDirection: 'column'
     },
     heartRateData: {
         flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flexDirection: 'column',
         borderRightWidth: moderateScale(2.5),
         borderColor: 'grey'
     },
     heartRateWave: {
-        flex: 4,
+        flex: 2,
+        flexDirection: 'column'
     },
     o2SatData: {
         flex: 1,
